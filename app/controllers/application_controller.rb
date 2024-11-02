12149e3ac80d0b1b7cc9d12d48d779d15783d452
ZephyRemix <hunyaochong@gmail.com>
2024-11-02 22:03:28 +0800

    Added left and right side bars on home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,18 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  layout :set_layout
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
   private
   def set_user
     @user = current_user
+  end
+
+  def set_layout
+    user_signed_in? ? "application" : "user_setup"
   end
 
   protected
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "home/index"
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   devise_scope :user do
     get "/users/sign_out" => "devise/sessions#destroy"
